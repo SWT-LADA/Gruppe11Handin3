290e8b1c0512466cd52fc721c7091cbca63f9de6
Line Drejer <l.n.drejer@gmail.com>
2020-04-22 15:59:48 +0200

    Tilføjede objekter til IT1

---
+++ Microwave.Test.Integration/IT1_ButtonDoor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class IT1_ButtonDoor
+    {
+        private Button _powerButton;
+        private Button _timeButton;
+        private Button _startCancelButton;
+        private Door _door;
+        private UserInterface _userInterface;
+        private ICookController _cookController;
+        private ILight _light;
+        private IDisplay _display;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _powerButton = new Button();
+            _timeButton = new Button();
+            _startCancelButton = new Button();
+            _door = new Door();
+            _cookController = Substitute.For<ICookController>();
+            _light = Substitute.For<ILight>();
+            _display = Substitute.For<IDisplay>();
+            _userInterface = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _light, _cookController);
+            
+        }
+
+        [Test]
+        public void Press_NoSubscribers_NoThrow()
+        {
+            int var = 1;
+        }
+    }
+}

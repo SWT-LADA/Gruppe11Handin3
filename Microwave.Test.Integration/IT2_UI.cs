7741daa292fe34448640c9dbaa10dc55a49f85c4
Anna Kjær Schmidt <au604464@uni.au.dk>
2020-04-24 10:16:39 +0200

    IT step 1 og 2

--- Microwave.Test.Integration/IT2_UI.cs
+++ Microwave.Test.Integration/IT2_UI.cs
@@ -7,22 +7,23 @@ using MicrowaveOvenClasses.Boundary;
 using MicrowaveOvenClasses.Controllers;
 using MicrowaveOvenClasses.Interfaces;
 using NSubstitute;
-using NSubstitute.ReceivedExtensions;
 using NUnit.Framework;
 
 namespace Microwave.Test.Integration
 {
     [TestFixture]
-    public class IT1_ButtonDoor
+    class IT2_UI
     {
         private Button _powerButton;
         private Button _timeButton;
         private Button _startCancelButton;
         private Door _door;
         private UserInterface _userInterface;
-        private ICookController _cookController;
+        private CookController _cookController;
+        private ITimer _timer;
         private ILight _light;
         private IDisplay _display;
+        private IPowerTube _powerTube;
 
         [SetUp]
         public void SetUp()
@@ -31,50 +32,47 @@ namespace Microwave.Test.Integration
             _timeButton = new Button();
             _startCancelButton = new Button();
             _door = new Door();
-            _cookController = Substitute.For<ICookController>();
+            _timer = Substitute.For<ITimer>();
             _light = Substitute.For<ILight>();
             _display = Substitute.For<IDisplay>();
+            _powerTube = Substitute.For<IPowerTube>();
+            _cookController = new CookController(_timer, _display, _powerTube);
             _userInterface = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _light, _cookController);
-        }
-
-        [Test]
-        public void DoorOpened()
-        {
-            _door.Open();
-            _light.Received().TurnOn();
-        }
-
-        [Test]
-        public void DoorClosed()
-        {
-            _door.Open();
-            _door.Close();
-            _light.Received().TurnOff();
-        }
-
-        [Test]
-        public void PowerButtonPressed()
-        {
-            _powerButton.Press();
-            _display.Received().ShowPower(50);
-        }
-
-        [Test]
-        public void TimeButtonPressed()
-        {
-            _powerButton.Press();
-            _timeButton.Press();
-            _display.Received().ShowTime(1,0);
-        }
-
-        [Test]
-        public void PressStartCancelButton()
-        {
+            
             _powerButton.Press();
             _timeButton.Press();
             _startCancelButton.Press();
-            _light.Received().TurnOn();
-            _cookController.Received().StartCooking(50,60);
+        }
+
+        [Test]
+        public void UIStartCooking()
+        {
+            _timer.Received().Start(60);
+            _powerTube.Received().TurnOn(50);
+        }
+
+        //[Test]
+        //public void CookControllerCookingIsDone()
+        //{
+        //   // _timer.Expired += Raise.EventWith(new System.Timers.); //her mangler vi at raise et event 
+        //    _display.Received().Clear();
+        //    _light.Received().TurnOff();
+        //}
+
+        [Test]
+        public void UIStopDoorOpened()
+        {
+            _door.Open();
+            _timer.Received().Stop();
+            _powerTube.Received().TurnOff();
+        }
+
+        [Test]
+        public void UIStopStartCancelPressed()
+        {
+            _startCancelButton.Press();
+            _timer.Received().Stop();
+            _powerTube.Received().TurnOff();
         }
     }
 }
